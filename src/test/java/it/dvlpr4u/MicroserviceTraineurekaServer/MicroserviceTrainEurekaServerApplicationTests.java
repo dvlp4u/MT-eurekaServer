24abd6a4da5c37a6ff9596335a91f233fbbcc651
S.La Malfa <ste.unibo@gmail.com>
2021-05-09 22:02:05 +0200

    First commit

---
+++ src/test/java/it/dvlpr4u/MicroserviceTraineurekaServer/MicroserviceTrainEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dvlpr4u.MicroserviceTraineurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTrainEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
